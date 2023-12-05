b9ba689f501d5070b071e732f3db8009f82fe8ee
itsyosefali <joeyxjoey123@gmail.com>
2023-12-05 11:33:23 +0200

    feat: new doctype shipping area

---
+++ libyanstore_addons/libyanstore_addons/doctype/shipping_area/shipping_area.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Ebkar and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Shipping Area', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ libyanstore_addons/libyanstore_addons/doctype/shipping_area/shipping_area.py
@@ -0,0 +1,7 @@
+import frappe
+from frappe.model.document import Document
+
+class ShippingArea(Document):
+    def validate(self):
+        if self.expected_delivery:
+        	self.expected_delivery = self.expected_delivery + " " + "Hours"
